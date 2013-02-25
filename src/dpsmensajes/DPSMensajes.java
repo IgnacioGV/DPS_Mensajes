6f0193e635825c1d4934965735d101f381f720cc
sisjose <antonio.ortiz@itcelaya.edu.mx>
2013-02-25 08:47:12 -0600

    Inicio Proyecto de Mensajes

---
+++ src/dpsmensajes/DPSMensajes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dpsmensajes;
+
+/**
+ *
+ * @author dw
+ */
+public class DPSMensajes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
